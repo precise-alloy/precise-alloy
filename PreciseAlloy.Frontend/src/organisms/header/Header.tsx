f180cda1a7898c8be943a311b8dd1c32654d847e
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-06 16:08:02 +0700

    [+] Add base style

--- PreciseAlloy.Frontend/src/organisms/header/Header.tsx
+++ PreciseAlloy.Frontend/src/organisms/header/Header.tsx
@@ -1,5 +1,18 @@
+import RequireCss from '@helpers/RequireCss';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="zzz-header">
+      <div className="zzz-container">
+        <div>
+          <a href="#">
+            <img className="zzz-header__logo" src="/assets/images/logo.svg" />
+          </a>
+        </div>
+      </div>
+      <RequireCss path="b-header" />
+    </header>
+  );
 };
 
 export default Header;
