5efd73d2a78d4ff1155b578e193ada662dd91c3e
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-10 14:05:25 +0700

    [+] Add .editorconfig

---
+++ PreciseAlloy.Web/Features/Components/AvatarBlock/AvatarBlockViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PreciseAlloy.Web.Features.Components.AvatarBlock;
+
+public class AvatarBlockViewModel
+    : BlockViewModel<Models.Blocks.AvatarBlock>
+{
+    public AvatarBlockViewModel(Models.Blocks.AvatarBlock currentBlock)
+        : base(currentBlock)
+    {
+    }
+}
