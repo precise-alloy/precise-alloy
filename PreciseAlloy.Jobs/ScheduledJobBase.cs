35480f92bb607ab7f59bf415f4c58117da449301
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-11 12:33:20 +0700

    [+] Add sample schedule job

--- PreciseAlloy.Jobs/ScheduledJobBase.cs
+++ PreciseAlloy.Jobs/ScheduledJobBase.cs
@@ -6,17 +6,17 @@ using PreciseAlloy.Utils.Extensions;
 namespace PreciseAlloy.Jobs;
 
 // ReSharper disable once UnusedMember.Global
-internal abstract class ScheduledJobBase
+public abstract class ScheduledJobBase
     : EPiServer.Scheduler.ScheduledJobBase
 {
     protected readonly ILogger<ScheduledJobBase> Logger;
     protected bool StopSignaled;
+    protected int Processed;
+    protected int Total;
+    protected int Succeeded;
+    protected int Failed;
+    protected string CurrentItem = string.Empty;
     private DateTime _lastNotificationTime = DateTime.UtcNow;
-    private int _processed;
-    private int _total;
-    private int _success;
-    private int _fail;
-    private string _currentItem = string.Empty;
 
     protected ScheduledJobBase(
         ILogger<ScheduledJobBase> logger)
@@ -35,11 +35,21 @@ internal abstract class ScheduledJobBase
 
     protected virtual string GetMessage(string? type)
     {
-        var stats = $"Processed: {_processed:N0} of {_total:N0} items. Succeeded: {_success:N0}. Failed: {_fail:N0}. Current item: {_currentItem}";
+        var stats = $"Processed: {Processed:N0} of {Total:N0} items. Succeeded: {Succeeded:N0}. Failed: {Failed:N0}. Current item: {CurrentItem}";
 
         return string.IsNullOrWhiteSpace(type)
             ? stats
             : $"{type}. {stats}";
+    }
+
+    protected virtual string GetStopMessage()
+    {
+        return GetMessage("STOPPED");
+    }
+
+    protected virtual string GetFinishMessage()
+    {
+        return GetMessage("FINISHED");
     }
 
     // ReSharper disable once UnusedMember.Global
