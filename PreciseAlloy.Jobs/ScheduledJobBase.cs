fc241a76e038519c265ffb7c3370b2f6d9079de3
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-11 09:31:42 +0700

    [+] Add Jobs

---
+++ PreciseAlloy.Jobs/ScheduledJobBase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using PreciseAlloy.Utils.Extensions;
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+#pragma warning disable CS0649
+
+namespace PreciseAlloy.Jobs;
+
+// ReSharper disable once UnusedMember.Global
+internal abstract class ScheduledJobBase
+    : EPiServer.Scheduler.ScheduledJobBase
+{
+    protected readonly ILogger Logger;
+    // ReSharper disable once InconsistentNaming
+    protected bool _stopSignaled;
+    private DateTime _lastNotificationTime = DateTime.UtcNow;
+    private int _processed;
+    private int _total;
+    private int _success;
+    private int _fail;
+    private string _currentItem = string.Empty;
+
+    protected ScheduledJobBase(
+        ILogger logger)
+    {
+        Logger = logger;
+        Logger.EnterConstructor();
+        IsStoppable = true;
+        Logger.ExitConstructor();
+    }
+
+    public override void Stop()
+    {
+        _stopSignaled = true;
+        Logger.LogInformation("Stop signaled");
+    }
+
+    protected virtual string GetMessage(string? type)
+    {
+        var stats = $"Processed: {_processed:N0} of {_total:N0} items. Succeeded: {_success:N0}. Failed: {_fail:N0}. Current item: {_currentItem}";
+
+        return string.IsNullOrWhiteSpace(type)
+            ? stats
+            : $"{type}. {stats}";
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    protected virtual void Report()
+    {
+        // Report every 5 seconds.
+        // This duration is long enough it does not cause performance issue
+        if ((DateTime.UtcNow - _lastNotificationTime).TotalSeconds < 5)
+        {
+            return;
+        }
+
+        _lastNotificationTime = DateTime.UtcNow;
+        OnStatusChanged(GetMessage(null));
+    }
+}
