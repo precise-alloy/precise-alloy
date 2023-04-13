852d56c6096ca4790e792ebb762b5cc4bb31609a
Nguyễn Tiến Dũng <dngtiennguyen17@gmail.com>
2023-04-13 11:48:40 +0700

    update tag

--- PreciseAlloy.Frontend/src/organisms/avatar/Avatar.tsx
+++ PreciseAlloy.Frontend/src/organisms/avatar/Avatar.tsx
@@ -6,20 +6,18 @@ const Avatar = (model: AvatarModel) => {
   const { image, name, jobTitle } = model;
 
   return (
-    <>
-      <div className="zzz-o-avatar">
-        <div className="zzz-o-avatar__image">
-          <Picture {...image} alt={name} />
-        </div>
-
-        <div className="zzz-o-avatar__info">
-          <h4 className="zzz-o-avatar__name">{name}</h4>
-
-          <div className="zzz-o-avatar__job-title">{jobTitle}</div>
-        </div>
-        <RequireCss path="b-avatar" />
+    <div className="zzz-o-avatar">
+      <div className="zzz-o-avatar__image">
+        <Picture {...image} />
       </div>
-    </>
+
+      <div className="zzz-o-avatar__info">
+        <h4 className="zzz-o-avatar__name">{name}</h4>
+
+        <div className="zzz-o-avatar__job-title">{jobTitle}</div>
+      </div>
+      <RequireCss path="b-avatar" />
+    </div>
   );
 };
 
