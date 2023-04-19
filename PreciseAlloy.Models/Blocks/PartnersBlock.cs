451990e889a08cf5668a5bd2a3e7448260138b68
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-19 17:56:13 +0700

    [+] Add Partner Block

--- PreciseAlloy.Models/Blocks/PartnersBlock.cs
+++ PreciseAlloy.Models/Blocks/PartnersBlock.cs
@@ -5,10 +5,10 @@ using PreciseAlloy.Models.Interfaces;
 namespace PreciseAlloy.Models.Blocks;
 
 [ContentType(
-    DisplayName = "Partner Block",
+    DisplayName = "Partners Block",
     Description = "",
     GUID = "d52552a4-9e55-4da8-bdc8-871741f1e27d")]
-public class PartnerBlock
+public class PartnersBlock
     : BaseBlockData, IHasHeading, IHasSubHeading
 {
     [Display(
