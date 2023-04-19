f8bac32d8facdb252b5d4e5579dbc26a7b31b1e7
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-19 16:30:29 +0700

    [+] Add Hero Block

---
+++ PreciseAlloy.Web/Features/Components/HeroBlock/HeroBlockViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PreciseAlloy.Web.Features.Components.HeroBlock;
+
+public class HeroBlockViewModel
+    : BlockViewModel<Models.Blocks.HeroBlock>
+{
+    public HeroBlockViewModel(Models.Blocks.HeroBlock currentBlock)
+        : base(currentBlock)
+    {
+    }
+}
