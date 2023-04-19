de70372f0c149870aacec18985d77e0bb1bf7b6a
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-04-19 08:38:41 +0700

    [*] Update editorconfig

--- PreciseAlloy.Models/Media/GifImage.cs
+++ PreciseAlloy.Models/Media/GifImage.cs
@@ -1,14 +1,13 @@
 using EPiServer.DataAnnotations;
 using EPiServer.Framework.DataAnnotations;
 
-namespace PreciseAlloy.Models.Media
+namespace PreciseAlloy.Models.Media;
+
+[ContentType(
+    GUID = "5A8960AE-75DA-426C-9002-1859E57AE882",
+    DisplayName = "GIF File",
+    Description = "")]
+[MediaDescriptor(ExtensionString = "gif")]
+public class GifImageFile : ImageFile
 {
-    [ContentType(
-        GUID = "5A8960AE-75DA-426C-9002-1859E57AE882",
-        DisplayName = "GIF File",
-        Description = "")]
-    [MediaDescriptor(ExtensionString = "gif")]
-    public class GifImageFile : ImageFile
-    {
-    }
 }
