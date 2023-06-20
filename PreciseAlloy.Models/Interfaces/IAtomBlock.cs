3b130596160ebe77e8f8aab58f8e5f40a09cfd23
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-06-20 13:49:57 +0700

    [*] Update comment

--- PreciseAlloy.Models/Interfaces/IAtomBlock.cs
+++ PreciseAlloy.Models/Interfaces/IAtomBlock.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Atom Block.
-/// Use for all the blocks that should should be used inside other blocks,
+/// Use for all the blocks that should be used inside other blocks,
 /// and shouldn't be added directly into page's content area
 /// </summary>
 public interface IAtomBlock
