0bc06dbe09c0f7adf6b4eb75b794cbea7f711cd2
Hung Le <hung.le.kim@ericsson.com>
2023-07-17 10:08:41 +0700

    feat: improve mock fetch with every url and fix error pl-states.json

--- PreciseAlloy.Frontend/mock-api/core/fetch.ts
+++ PreciseAlloy.Frontend/mock-api/core/fetch.ts
@@ -1,7 +1,25 @@
 import center from "./event";
 
-export default function mockFetch(url: URL, req?: RequestInit): Promise<Response> {
-  const response = center.dispatch({ url, req });
+export default function mockFetch(input: URL | RequestInfo, req?: RequestInit): Promise<Response> {
+  if (typeof input === 'string') {
+    input = new URL(input, window.location.origin);
+  }
+
+  if (input instanceof Request) {
+    console.log(`${input.url}: you're using origin fetch`);
+
+    return window.originFetch(input, req);
+  }
+
+  const response = center.dispatch({ url: input, req });
+
+  if (response.status && response.status === 404) {
+    console.log(`${input.href}: The api is not exists in mock, fallback to origin fetch`);
+
+    return window.originFetch(input, req);
+  }
+
+  console.log(`${input.href}: you're using mock fetch`);
 
   return new Promise(resolve => {
     resolve(
