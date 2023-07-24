9ab11454170c73cd8e03f81201559a2d0f88013e
Tuyen Pham <vantuyen.pham@optimizely.com>
2023-07-24 15:15:36 +0700

    [*] Update pipeline

--- PreciseAlloy.Frontend/.build/integration.ps1
+++ PreciseAlloy.Frontend/.build/integration.ps1
@@ -1,20 +1,42 @@
 Write-Output "Script is running"
 
+$apiVersion = '6.0'
+
+function Get-Teams {
+  param(
+    [Parameter(Mandatory = $true)] [String]$apisUrl,
+    [Parameter(Mandatory = $true)] $headers
+  )
+
+  $requestUrl = '{0}/teams' -f $apisUrl
+  $requestUrl = '{0}?api-version={1}' -f $requestUrl, $apiVersion
+
+  $debugInfo = 'GET {0}' -f $requestUrl
+  Write-Host $debugInfo
+
+  $responseData = Invoke-RestMethod `
+    -Method GET `
+    -Headers $headers `
+    -Uri $requestUrl
+  
+  $responseData.value | Select-Object -Property Id, Name
+}
+
 function Get-ExistingOrNewPullRequest {
   param(
     [Parameter(Mandatory = $true)] [String]$sourceBranch,
     [Parameter(Mandatory = $true)] [String]$targetBranch,
-    [Parameter(Mandatory = $true)] [String]$projectUrl,
+    [Parameter(Mandatory = $true)] [String]$repoUrl,
     [Parameter(Mandatory = $true)] [String]$reviewerId,
     [Parameter(Mandatory = $true)] $headers
   )
 
-  $requestUrl = '{0}/pullRequests' -f $projectUrl
+  $requestUrl = '{0}/pullRequests' -f $repoUrl
   $requestUrl = '{0}?searchCriteria.sourceRefName=refs/heads/{1}' -f $requestUrl, $sourceBranch
   $requestUrl = '{0}&searchCriteria.targetRefName=refs/heads/{1}' -f $requestUrl, $targetBranch
   $requestUrl = '{0}&searchCriteria.status=active' -f $requestUrl
   $requestUrl = '{0}&$top=1' -f $requestUrl
-  $requestUrl = '{0}&api-version=6.0' -f $requestUrl
+  $requestUrl = '{0}&api-version={1}' -f $requestUrl, $apiVersion
 
   Write-Output "Sending a REST call to get existing pull request from $sourceBranch to $targetBranch"
 
@@ -35,7 +57,7 @@ function Get-ExistingOrNewPullRequest {
     New-PullRequest `
       -sourceBranch $sourceBranch `
       -targetBranch $targetBranch `
-      -projectUrl $projectUrl `
+      -repoUrl $repoUrl `
       -reviewerId $reviewerId `
       -setAutoComplete `
       -headers $headers
@@ -46,13 +68,13 @@ function New-PullRequest {
   param(
     [Parameter(Mandatory = $true)] [String]$sourceBranch,
     [Parameter(Mandatory = $true)] [String]$targetBranch,
-    [Parameter(Mandatory = $true)] [String]$projectUrl,
+    [Parameter(Mandatory = $true)] [String]$repoUrl,
     [Parameter(Mandatory = $true)] [String]$reviewerId,
     [Parameter()] [Switch]$setAutoComplete,
     [Parameter(Mandatory = $true)] $headers
   )
 
-  $requestUrl = '{0}/pullrequests?api-version=6.0' -f $projectUrl
+  $requestUrl = '{0}/pullrequests?api-version={1}' -f $repoUrl, $apiVersion
   $requestData = @{
     "sourceRefName" = "refs/heads/$sourceBranch"
     "targetRefName" = "refs/heads/$targetBranch"
@@ -82,7 +104,7 @@ function New-PullRequest {
   
   if ($setAutoComplete) {
     Set-AutoComplete `
-      -projectUrl $projectUrl `
+      -repoUrl $repoUrl `
       -pullRequestId $pullRequestId `
       -autoCompleteSetBy $pullRequestCreatorId `
       -headers $headers
@@ -91,13 +113,13 @@ function New-PullRequest {
 
 function Set-AutoComplete {
   param(
-    [Parameter(Mandatory = $true)] [String]$projectUrl,
+    [Parameter(Mandatory = $true)] [String]$repoUrl,
     [Parameter(Mandatory = $true)] [String]$pullRequestId,
     [Parameter(Mandatory = $true)] [String]$autoCompleteSetBy,
     [Parameter(Mandatory = $true)] $headers
   )
 
-  $requestUrl = '{0}/pullRequests/{1}?api-version=6.0' -f $projectUrl, $pullRequestId
+  $requestUrl = '{0}/pullRequests/{1}?api-version={1}' -f $repoUrl, $pullRequestId, $apiVersion
   $requestData = @{
     "autoCompleteSetBy" = @{
       "id" = $autoCompleteSetBy
@@ -122,30 +144,41 @@ function Set-AutoComplete {
 }
 
 function Request-PullToBaseBranch {
+  $teamFoundationCollectionUri = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
+  $teamProject = $env:SYSTEM_TEAMPROJECT
   $repositoryName = $env:CUSTOM_REPOSITORY_NAME
   $sourceBranch = $env:CUSTOM_SOURCE_BRANCH
   $targetBranch = $env:CUSTOM_TARGET_BRANCH
   $reviewerId = $env:CUSTOM_REVIEWER_ID
   $accessToken = $env:SYSTEM_ACCESSTOKEN
 
+  Write-Host "teamFoundationCollectionUri: '$teamFoundationCollectionUri'" -ForegroundColor DarkYellow
+  Write-Host "teamProject: '$teamProject'" -ForegroundColor DarkYellow
   Write-Host "repositoryName: '$repositoryName'" -ForegroundColor DarkYellow
   Write-Host "sourceBranch: '$sourceBranch'" -ForegroundColor DarkYellow
   Write-Host "targetBranch: '$targetBranch'" -ForegroundColor DarkYellow
   Write-Host "reviewerId: '$reviewerId'" -ForegroundColor DarkYellow
 
   # construct base URLs
-  $apisUrl = "{0}/{1}/_apis" -f $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, $env:SYSTEM_TEAMPROJECT
-  $projectUrl = "{0}/git/repositories/{1}" -f $apisUrl, $repositoryName
+  if ($teamFoundationCollectionUri -notmatch '/$') {
+    $teamFoundationCollectionUri += '/'
+  }
+  $apisUrl = "{0}{1}/_apis" -f $teamFoundationCollectionUri, $teamProject
+  $repoUrl = "{0}/git/repositories/{1}" -f $apisUrl, $repositoryName
 
   # create common headers
   $headers = @{}
   $headers.Add("Authorization", "Bearer $accessToken")
   $headers.Add("Content-Type", "application/json")
 
+  # Get-Teams `
+  #   -apisUrl $apisUrl `
+  #   -headers $headers
+
   Get-ExistingOrNewPullRequest `
     -sourceBranch $sourceBranch `
     -targetBranch $targetBranch `
-    -projectUrl $projectUrl `
+    -repoUrl $repoUrl `
     -reviewerId $reviewerId `
     -headers $headers
 }
