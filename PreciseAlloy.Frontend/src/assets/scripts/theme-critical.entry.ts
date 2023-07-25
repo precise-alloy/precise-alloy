b7fa53cec3034cd8f0ffb5127f06b3ef4cd61e69
Hung Le <hung.le.kim@ericsson.com>
2023-07-25 10:52:14 +0700

    fix: Icon theme not show at first load

--- PreciseAlloy.Frontend/src/assets/scripts/theme-critical.entry.ts
+++ PreciseAlloy.Frontend/src/assets/scripts/theme-critical.entry.ts
@@ -1,15 +1,19 @@
 (() => {
   // enforce local storage setting but also fallback to user-agent preferences
 
+  const THEME_KEY = 'MSG_THEME';
+
   const urlParams = new URLSearchParams(window.location.search);
   const theme = urlParams.get('theme');
   if (theme === 'dark' || theme == 'light') {
+    localStorage.setItem(THEME_KEY, theme);
     document.documentElement.setAttribute('data-theme', theme);
     return;
   }
 
-  if (localStorage.getItem('theme') === 'dark'
-    || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  if (localStorage.getItem(THEME_KEY) === 'dark'
+    || (!localStorage.getItem(THEME_KEY) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    localStorage.setItem(THEME_KEY, 'dark');
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 })();
