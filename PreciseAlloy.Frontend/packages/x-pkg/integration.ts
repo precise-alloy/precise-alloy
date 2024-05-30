76834d0a450a695907292bff9b269893b81ed8d1
Hung Le <hung.le.kim@ericsson.com>
2024-05-30 10:38:31 +0700

    feat: update start command

---
+++ PreciseAlloy.Frontend/packages/x-pkg/integration.ts
@@ -0,0 +1,100 @@
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import slash from 'slash';
+import _ from 'lodash';
+import { glob } from 'glob';
+import crypto from 'node:crypto';
+import { loadEnv } from 'vite';
+
+interface CopyItem {
+  from: string;
+  to?: string;
+}
+const argvModeIndex = process.argv.indexOf('--mode');
+const mode = argvModeIndex >= 0 && argvModeIndex < process.argv.length - 1 && !process.argv[argvModeIndex + 1].startsWith('-') ? process.argv[argvModeIndex + 1] : 'production';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const xpackEnv = loadEnv(mode, __dirname);
+const toAbsolute = (p: string) => slash(path.resolve(__dirname, p));
+const log = console.log.bind(console);
+
+const hashes: { [path: string]: string } = {};
+const staticBasePath = toAbsolute('dist/static');
+const srcBasePath = toAbsolute('dist/static/assets');
+const destBasePath = toAbsolute(xpackEnv.VITE_INTE_ASSET_DIR);
+const patternPath = xpackEnv.VITE_INTE_PATTERN_DIR ? toAbsolute(xpackEnv.VITE_INTE_PATTERN_DIR) : undefined;
+
+const copyItems: CopyItem[] = [
+  { from: 'css' },
+  { from: 'fonts' },
+  { from: 'images' },
+  { from: 'js' },
+  { from: 'vendors' },
+  { from: 'hashes.json' },
+  { from: 'pages', to: patternPath },
+];
+const hashItems: string[] = ['css', 'images', 'js'];
+
+copyItems.forEach((item) => {
+  const srcPath = slash(path.join(srcBasePath, item.from));
+  const destPath = slash(item.to ?? path.join(destBasePath, item.from));
+
+  if (!fs.existsSync(srcPath)) {
+    return;
+  }
+
+  log(`Copy file ${srcPath} to ${destPath}`);
+
+  if (fs.statSync(srcPath).isDirectory()) {
+    if (fs.existsSync(destPath)) {
+      fs.rmSync(destPath, { recursive: true, force: true });
+    }
+
+    fs.mkdirSync(destPath, { recursive: true });
+
+    fs.cpSync(srcPath, destPath, { recursive: true, force: true });
+  } else {
+    const destDirPath = path.dirname(destPath);
+    if (!fs.existsSync(destDirPath)) {
+      fs.mkdirSync(destDirPath);
+    }
+
+    fs.copyFileSync(srcPath, destPath);
+  }
+});
+
+hashItems.forEach((item) => {
+  const srcPath = slash(path.join(srcBasePath, item));
+  const files = glob.sync(srcPath + '/**/*.{css,js,svg}');
+  files.forEach((file) => {
+    const relativePath = slash(file.substring(staticBasePath.length));
+
+    if (!/\.0x[a-z0-9]{8}\.\w+$/gi.test(file)) {
+      const content = fs.readFileSync(file);
+      const sha1Hash = crypto.createHash('sha1');
+      sha1Hash.update(content);
+      const hash = sha1Hash.digest('base64url').substring(0, 10);
+      hashes[relativePath] = hash;
+    } else {
+      hashes[relativePath] = '';
+    }
+  });
+});
+
+fs.writeFileSync(path.join(destBasePath, 'hashes.json'), JSON.stringify(hashes, null, '  '));
+
+if (patternPath) {
+  fs.mkdirSync(patternPath, { recursive: true });
+  glob.sync('./dist/static/{atoms,molecules,organisms,templates,pages}/**/*.*').forEach((p) => {
+    const basename = path.basename(slash(p).replaceAll(/(atoms|molecules|organisms|templates|pages)\/([\w._-]+)$/gi, '$1-$2'));
+    fs.copyFileSync(p, resolve(patternPath, basename));
+  });
+
+  glob.sync(slash(path.resolve(patternPath + '/**/*.{htm,html}'))).forEach((p) => {
+    const text = fs.readFileSync(p, 'utf-8');
+    const newText = text.replaceAll(/react-loader\.\w+\.js/gi, 'react-loader.0x00000000.js');
+    if (text !== newText) {
+      fs.writeFileSync(p, newText);
+    }
+  });
+}
---
+++ PreciseAlloy.Frontend/packages/x-pkg/prerender.ts
@@ -0,0 +1,214 @@
+// Pre-render the app into static HTML.
+// run `npm run generate` and then `dist/static` can be served as a static site.
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
+import jsBeautify, { CSSBeautifyOptions, HTMLBeautifyOptions, JSBeautifyOptions } from 'js-beautify';
+import * as cheerio from 'cheerio';
+import crypto from 'node:crypto';
+import slash from 'slash';
+import _ from 'lodash';
+import { loadEnv } from 'vite';
+import chalk from 'chalk';
+
+interface Route {
+  name: string;
+  route: string;
+}
+
+interface RenderedPage {
+  name: string;
+  url: string;
+  fileName: string;
+}
+const argvModeIndex = process.argv.indexOf('--mode');
+const mode =
+  argvModeIndex >= 0 && argvModeIndex < process.argv.length - 1 && !process.argv[argvModeIndex + 1].startsWith('-')
+    ? process.argv[argvModeIndex + 1]
+    : 'production';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const xpackEnv = loadEnv(mode, __dirname);
+const toAbsolute = (p: string) => path.resolve(__dirname, p);
+const log = console.log.bind(console);
+
+const template = fs.readFileSync(toAbsolute(process.env.VITE_TEMPLATE ?? 'dist/static/index.html'), 'utf-8');
+const { render } = await import(pathToFileURL(toAbsolute('./dist/server/entry-server.js')).href);
+
+const beautifyOptions: HTMLBeautifyOptions | JSBeautifyOptions | CSSBeautifyOptions = {
+  indent_size: 2,
+  indent_char: ' ',
+  keep_array_indentation: false,
+  break_chained_methods: false,
+  indent_scripts: 'normal',
+  brace_style: 'collapse',
+  space_before_conditional: true,
+  unescape_strings: false,
+  jslint_happy: false,
+  end_with_newline: false,
+  wrap_line_length: 0,
+  indent_inner_html: false,
+  comma_first: false,
+  e4x: false,
+  indent_empty_lines: false,
+  wrap_attributes: 'force',
+  max_preserve_newlines: 5,
+  preserve_newlines: true,
+};
+
+// determine routes to pre-render from src/pages
+const routesToPrerender = fs
+  .readdirSync(toAbsolute('src/pages'))
+  .map((file): Route => {
+    const name = path.parse(file).name;
+    const normalizedName = name.replaceAll(/^(\w+)/gi, (_p0, p1: string) => _.lowerCase(_.lowerCase(p1)).replaceAll(' ', '-'));
+
+    return {
+      name: _.startCase(_.lowerCase(name)),
+      route: name === 'Root' ? '/' : `/pages/${normalizedName}`,
+    };
+  })
+  .filter((r) => r.name != 'root');
+
+routesToPrerender.push({
+  name: 'Root',
+  route: '/',
+});
+
+const updateResourcePath = ($: cheerio.CheerioAPI, tagName: string, attr: string, addHash: boolean) => {
+  $(tagName).each((_, el) => {
+    const href = $(el).attr(attr);
+
+    if (href && href.startsWith('/')) {
+      let newPath = href;
+
+      if (process.env.VITE_DOMAIN) {
+        newPath = process.env.VITE_DOMAIN + newPath;
+      }
+
+      if (
+        href.startsWith(xpackEnv.VITE_BASE_URL) &&
+        !href.startsWith(xpackEnv.VITE_BASE_URL + 'assets/vendors/') &&
+        ['.css', '.ico', '.js', '.webmanifest', '.svg'].includes(path.extname(href).toLowerCase()) &&
+        !/\.0x[a-z0-9]{8}\.\w+$/gi.test(href)
+      ) {
+        const path = toAbsolute('dist/static/' + href.substring(xpackEnv.VITE_BASE_URL.length));
+        if (fs.existsSync(path)) {
+          const content = fs.readFileSync(path);
+          const sha1Hash = crypto.createHash('sha1');
+          sha1Hash.update(content);
+          const hash = sha1Hash.digest('base64url').substring(0, 10);
+
+          if (addHash) {
+            newPath += '?v=' + hash;
+          }
+        } else {
+          // Log warning
+          log(chalk.yellow('Cannot find:', path));
+        }
+      }
+
+      if (newPath != href) {
+        $(el).attr(attr, newPath);
+      }
+    }
+  });
+};
+
+const removeDuplicateAssets = ($: cheerio.CheerioAPI, selector: string, attr: string, paths: string[]) => {
+  $(selector).each((_, el) => {
+    if ($(el).attr('data-pl-inplace') === 'true') {
+      return;
+    }
+
+    const path = $(el).attr(attr);
+    if (!path) {
+      return;
+    }
+
+    const index = $(el).index();
+    const parent = $(el).parent().clone();
+    const child = parent.children()[index];
+    parent.empty();
+    parent.append(child);
+    const html = parent.html();
+
+    $(el).after('\n<!-- ' + html + ' -->');
+
+    if (paths.includes(path)) {
+      $(el).remove();
+      return;
+    }
+
+    paths.push(path);
+    $('head').append(el);
+  });
+};
+
+const viteAbsoluteUrl = (remain: string, addExtension = false): string => {
+  const baseUrl = xpackEnv.VITE_BASE_URL;
+  const normalizedRemain =
+    (remain?.startsWith('/') ? remain : '/' + remain) + (addExtension && !remain.endsWith('/') ? xpackEnv.VITE_PATH_EXTENSION ?? '' : '');
+  if (!baseUrl) {
+    return normalizedRemain;
+  }
+
+  if (!baseUrl.endsWith('/')) {
+    return baseUrl + normalizedRemain;
+  }
+
+  const len = baseUrl.length;
+  return baseUrl.substring(0, len - 1) + normalizedRemain;
+};
+
+const renderPage = async (renderedPages: RenderedPage[], addHash: boolean) => {
+  // pre-render each route...
+  for (const route of routesToPrerender) {
+    const output = await render(viteAbsoluteUrl(route.route, true));
+
+    const destLocalizedFolderPath = toAbsolute('dist/static');
+
+    let html = template.replace(`<!--app-html-->`, output.html ?? '').replace('@style.scss', '/assets/css/' + route.name + '.css');
+    const $ = cheerio.load(html);
+    const paths: string[] = [];
+    removeDuplicateAssets($, 'link[data-pl-require][href]', 'href', paths);
+    removeDuplicateAssets($, 'script[data-pl-require][src]', 'src', paths);
+    updateResourcePath($, 'link', 'href', addHash);
+    updateResourcePath($, 'script', 'src', addHash);
+    updateResourcePath($, 'img', 'src', addHash);
+
+    $('head title').text(route.name);
+
+    const fileName = (route.route === '/' ? '/index' : route.route) + '.html';
+    const filePath = `${destLocalizedFolderPath}${fileName}`;
+
+    if (!fs.existsSync(path.dirname(filePath))) {
+      fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    }
+
+    html = $.html();
+
+    html = jsBeautify.html_beautify(html, beautifyOptions);
+    html = html.replace('/* app-styles */', output.styles);
+
+    fs.writeFileSync(toAbsolute(filePath), html);
+    log('pre-rendered:', slash(filePath));
+
+    renderedPages.push({
+      name: _.kebabCase(fileName.replaceAll(/\.\w+$/gi, '')),
+      url: `${process.env.VITE_DOMAIN ?? ''}${fileName}`,
+      fileName: fileName,
+    });
+  }
+};
+
+(async () => {
+  const renderedPages: RenderedPage[] = [];
+  const pool: Promise<unknown>[] = [];
+
+  const addHash = !!process.argv.includes('--add-hash');
+  pool.push(renderPage(renderedPages, addHash));
+
+  await Promise.all(pool);
+})();
