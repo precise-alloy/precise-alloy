3be65304a3872234757cc3337ea528b34ffa992f
tuyen-at-work <tuyen.at.work@gmail.com>
2024-06-10 11:44:46 +0700

    build: update package versions

---
+++ PreciseAlloy.Services.Tests/SampleTests.cs
@@ -0,0 +1,18 @@
+﻿namespace PreciseAlloy.Services.Tests;
+
+public class SampleTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var a = 1;
+        var b = 2;
+
+        // Act
+        var result = a + b;
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
