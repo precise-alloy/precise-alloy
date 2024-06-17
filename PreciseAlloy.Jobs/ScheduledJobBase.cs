a0c693f6932420bcb5a0e7e148ee2bb6b8373c25
tuyen-at-work <tuyen.at.work@gmail.com>
2024-06-17 10:00:21 +0700

    feat: update schedule job base

--- PreciseAlloy.Jobs/ScheduledJobBase.cs
+++ PreciseAlloy.Jobs/ScheduledJobBase.cs
@@ -5,20 +5,53 @@ using PreciseAlloy.Utils.Extensions;
 
 namespace PreciseAlloy.Jobs;
 
-// ReSharper disable once UnusedMember.Global
+/// <summary>
+/// The base class for scheduled jobs.
+/// </summary>
 public abstract class ScheduledJobBase
     : EPiServer.Scheduler.ScheduledJobBase
 {
     private DateTime _lastNotificationTime = DateTime.UtcNow;
-    
+
+    /// <summary>
+    /// The logger.
+    /// </summary>
     protected readonly ILogger<ScheduledJobBase> Logger;
+
+    /// <summary>
+    /// The flag to indicate if the job is waiting to be stopped.
+    /// </summary>
     protected bool StopSignaled { get; private set; }
+
+    /// <summary>
+    /// The number of items processed.
+    /// </summary>
     protected int Processed { get; set; }
+
+    /// <summary>
+    /// The total number of items to process.
+    /// </summary>
     protected int Total { get; set; }
+
+    /// <summary>
+    /// The number of items that succeeded.
+    /// </summary>
     protected int Succeeded { get; set; }
+
+    /// <summary>
+    /// The number of items that failed.
+    /// </summary>
     protected int Failed { get; set; }
+
+    /// <summary>
+    /// The current item being processed.
+    /// </summary>
     protected string? CurrentItem { get; set; }
 
+    /// <summary>
+    /// Initialize a new instance of the <see cref="ScheduledJobBase"/> class.
+    /// </summary>
+    /// <param name="logger"></param>
     protected ScheduledJobBase(
         ILogger<ScheduledJobBase> logger)
     {
@@ -28,12 +61,20 @@ public abstract class ScheduledJobBase
         Logger.ExitConstructor();
     }
 
+    /// <summary>
+    /// Set the `StopSignaled` flag to true.
+    /// </summary>
     public override void Stop()
     {
         StopSignaled = true;
         Logger.LogInformation("Stop signaled");
     }
 
+    /// <summary>
+    /// Get the current status of the job.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     protected virtual string GetMessage(string? type)
     {
         return (!string.IsNullOrWhiteSpace(type) ? type + ". " : "")
@@ -43,16 +84,29 @@ public abstract class ScheduledJobBase
                + $"Current item: {CurrentItem ?? "???"}.";
     }
 
+    /// <summary>
+    /// Get the current status of the job, and indicate that the job has stopped.
+    /// </summary>
+    /// <returns></returns>
+    // ReSharper disable once UnusedMember.Global
     protected virtual string GetStopMessage()
     {
         return GetMessage("STOPPED");
     }
 
+    /// <summary>
+    /// Get the current status of the job, and indicate that the job has finished.
+    /// </summary>
+    /// <returns></returns>
+    // ReSharper disable once UnusedMember.Global
     protected virtual string GetFinishMessage()
     {
         return GetMessage("FINISHED");
     }
 
+    /// <summary>
+    /// Report the current status of the job.
+    /// </summary>
     // ReSharper disable once UnusedMember.Global
     protected virtual void Report()
     {
