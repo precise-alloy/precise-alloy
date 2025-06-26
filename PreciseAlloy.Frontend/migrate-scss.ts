c8e7d7715dc65d54ec9d04c92031ecd7244f6b0b
DinhPham <dinh.pham@optimizely.com>
2025-06-26 15:08:02 +0700

    chore: migrate scss - to remove manual migration

--- PreciseAlloy.Frontend/migrate-scss.ts
+++ PreciseAlloy.Frontend/migrate-scss.ts
@@ -1,14 +1,10 @@
-import fs from 'fs/promises';
-import path from 'path';
 import { glob } from 'glob';
-import slash from 'slash';
 import { execSync } from 'child_process';
 
-const automaticMigration = async () => {
+const migration = async () => {
   const baseFiles = [
     './src/assets/styles/style-base.scss',
     './src/assets/styles/style.all.scss',
-    './src/assets/styles/_additional-data.scss',
     './xpack/styles/root.scss',
   ];
 
@@ -43,58 +39,4 @@ const automaticMigration = async () => {
   }
 }
 
-const manualMigration = async () => {
-  const abstractsPath = 'src/assets/styles/00-abstracts/abstracts';
-  const mixinsPath = 'src/assets/styles/01-mixins/mixins';
-
-  const manualSassMigrationFiles = await glob(
-    [
-      'src/assets/styles/01-mixins/**/*.scss',
-      'xpack/styles/**/*.scss',
-      'src/assets/styles/02-base/**/*.scss',
-    ],
-    {
-      nodir: true,
-    }
-  );
-
-  for (const file of manualSassMigrationFiles) {
-    const content = await fs.readFile(file, 'utf-8');
-
-    if (file.includes('00-abstracts') || content.includes('00-abstracts/abstracts')) {
-      continue;
-    }
-
-    const usesVars = /\$[a-zA-Z0-9_-]+\b/.test(content);
-    const usesMixins = /@include\s+[a-zA-Z0-9_-]+\b/.test(content);
-    const usesFunctions = /[a-zA-Z0-9_-]+\s*\([^)]*\)/.test(content);
-
-    if (usesVars || usesMixins || usesFunctions) {
-      const useLines = [];
-      if (file.includes('01-mixins') || content.includes('01-mixins/mixins')) {
-        const relToAbstracts = slash(path.relative(path.dirname(file), abstractsPath));
-        useLines.push(`@use '${relToAbstracts}' as *;`);
-      } else {
-        if (usesVars) {
-          const relToAbstracts = slash(path.relative(path.dirname(file), abstractsPath));
-          useLines.push(`@use '${relToAbstracts}' as *;`);
-        }
-
-        if (usesMixins || usesFunctions) {
-          const relToMixins = slash(path.relative(path.dirname(file), mixinsPath));
-          useLines.push(`@use '${relToMixins}' as *;`);
-        }
-      }
-
-      const updated = useLines.join('\n') + '\n\n' + content;
-      await fs.writeFile(file, updated, 'utf-8');
-    }
-  }
-}
-
-const run = async () => {
- await automaticMigration();
- await manualMigration();
-};
-
-run();
+migration();
