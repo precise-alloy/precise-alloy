1849029ba7b3f0d85f576402cc75ca3b4f0adf67
tuyen-at-work <tuyen.at.work@gmail.com>
2025-06-30 09:56:52 +0700

    feat: robots headers

--- PreciseAlloy.Web/Infrastructure/Middlewares/RobotsHeaderMiddleware.cs
+++ PreciseAlloy.Web/Infrastructure/Middlewares/RobotsHeaderMiddleware.cs
@@ -3,6 +3,16 @@ using PreciseAlloy.Web.Infrastructure.Models;
 
 namespace PreciseAlloy.Web.Infrastructure.Middlewares;
 
+/// <summary>
+///     Middleware that conditionally adds an <c>x-robots-tag</c> HTTP header to responses,
+///     instructing search engines not to index, follow, cache, or archive pages.
+///     The header is applied if the application is not running in production, or if the current
+///     request's host matches any domain specified in <see cref="SeoOptions.NoRobotsDomains" />.
+///     This helps prevent search engines from indexing non-production environments or specific domains.
+/// </summary>
+/// <param name="next"></param>
+/// <param name="webHostEnvironment"></param>
+/// <param name="seoOptions"></param>
 public class RobotsHeaderMiddleware(
     RequestDelegate next,
     IWebHostEnvironment webHostEnvironment,
@@ -11,13 +21,13 @@ public class RobotsHeaderMiddleware(
     public async Task Invoke(
         HttpContext httpContext)
     {
-        bool blockRobots = !webHostEnvironment.IsProduction()
-                           || seoOptions
-                               .Value
-                               ?.NoRobotsDomains
-                               ?.Any(d => d.Equals(httpContext.Request.Host.Host, StringComparison.OrdinalIgnoreCase)
-                                          || (d.StartsWith('.')
-                                              && httpContext.Request.Host.Host.EndsWith(d, StringComparison.OrdinalIgnoreCase))) == true;
+        var blockRobots = !webHostEnvironment.IsProduction()
+                          || seoOptions
+                              .Value
+                              ?.NoRobotsDomains
+                              ?.Any(d => d.Equals(httpContext.Request.Host.Host, StringComparison.OrdinalIgnoreCase)
+                                         || (d.StartsWith('.')
+                                             && httpContext.Request.Host.Host.EndsWith(d, StringComparison.OrdinalIgnoreCase))) == true;
 
         if (blockRobots)
         {
