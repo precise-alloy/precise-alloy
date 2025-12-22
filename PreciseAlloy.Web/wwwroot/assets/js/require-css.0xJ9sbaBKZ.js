5d3ade7f9ee980b1d4131ed766fe48127daaf830
Build Service <build-service@eshn.dev>
2025-12-22 07:18:33 +0000

    Create release package

---
+++ PreciseAlloy.Web/wwwroot/assets/js/require-css.0xJ9sbaBKZ.js
@@ -0,0 +1,2 @@
+import{j as o}from"./react-loader.0xCSF-6dlM.js";import{v as e}from"./functions.0xBot6-XZv.js";const n=({path:s,rel:t})=>{const r=/^https?:\/\//gi.test(s)?s:s.startsWith("vendors/")?e("/assets/"+s+".css"):e("/assets/css/"+s+".css");return o.jsx("link",{"data-pl-require":!0,href:r,rel:t??"stylesheet"})};export{n as R};
+//# sourceMappingURL=require-css.0xJ9sbaBKZ.js.map
