70832c3202af52939a3284cd452c33ad2b873cf2
tuyen-at-work <tuyen.at.work@gmail.com>
2025-12-24 14:56:19 +0700

    fix: static renderer

--- PreciseAlloy.Frontend/xpack/root/index.tsx
+++ PreciseAlloy.Frontend/xpack/root/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { RootModel, SinglePageNode } from '@_types/types';
 import { useEffect, useMemo, useState } from 'react';
 import { viteAbsoluteUrl } from '@helpers/functions';
@@ -8,7 +10,7 @@ import ActiveItemOptions from './active-item-options';
 import RootNav from './root-nav';
 
 export default function Root(props: RootModel) {
-  const [activeItem, setActiveItem] = useState<SinglePageNode>();
+  const [activeItem, setActiveItem] = useState<SinglePageNode | undefined>(typeof window !== 'undefined' ? undefined : undefined);
   const [isTopPanel, setTopPanel] = useState<boolean>(() =>
     typeof localStorage !== 'undefined' ? localStorage.getItem('MSG_IS_TOP_PANEL') === 'true' : false
   );
