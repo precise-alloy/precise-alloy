10088c1435ad5d51b051761ad70a5d9edefc17c8
Build Service <build-service@eshn.dev>
2026-05-11 09:51:09 +0000

    chore(assets): create frontend release package

---
+++ PreciseAlloy.Web/wwwroot/assets/js/require-css.0xDjmw8Ok9.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime.0xPpiJVzXi.js";var t=e(),n=({path:e,rel:n})=>(0,t.jsx)(`link`,{"data-pl-require":!0,href:/^https?:\/\//gi.test(e)?e:e.startsWith(`vendors/`)?viteAbsoluteUrl(`/assets/`+e+`.css`):viteAbsoluteUrl(`/assets/css/`+e+`.css`),rel:n??`stylesheet`});export{n as t};
+//# sourceMappingURL=require-css.0xDjmw8Ok9.js.map
